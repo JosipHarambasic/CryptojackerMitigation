6ee99d370fc789d20e045404c938f849fdcf88b9
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2022-12-17 23:26:57 +0100

    update

--- main.py
+++ main.py
@@ -1,51 +1,46 @@
 import subprocess
-
-# sudo /usr/libexec/locate.updatedb
 import time
-
 from Parser import Parser
 
-
 def mitigateCryptojacker():
-    outputlist = []
-    # Iterate over all the servers in the list and ping each server
-    sudo_password = '1230'
-    command = 'updatedb'
-    command = command.split()
-
-    # Parse the output file that could have malicious programs
 
     # We need to update the db to locate the actual target file
-    cmd1 = subprocess.Popen(['echo', sudo_password], stdout=subprocess.PIPE)
-    # subprocess.Popen(['sudo', '-S'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+    subprocess.Popen("rm nethogs.txt", shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen("updatedb", shell=True, stdout=subprocess.PIPE)
+
+    print("started networktracking")
+    startNetworkTracking()
+    print("finished networktracking")
 
     maliciousPrograms = Parser("whitelist.txt", "nethogs.txt").parse()
-    for i in maliciousPrograms:
-        # cpu limit 'cpulimit -p PID -l 10'
-        print(maliciousPrograms[i])
-        subprocess.Popen("cpulimit -p " + maliciousPrograms[i] + " -l 10")
-        # subprocess.Popen('kill -9' + str(i), shell=True, stdout=subprocess.PIPE)
+    if len(maliciousPrograms) > 0:
+        for i in maliciousPrograms:
+            # cpu limit 'cpulimit -p PID -l 10'
+            print(maliciousPrograms[i])
+            print(i)
+            subprocess.Popen("cpulimit -p " + maliciousPrograms[i] + " -l 10 -b", shell=True, stdout=subprocess.PIPE)
+            time.sleep(10)
+            subprocess.Popen("kill -9 " + maliciousPrograms[i], shell=True, stdin=subprocess.PIPE)
+            print("killed process: " + i + " with PID: " + maliciousPrograms[i])
 
-    temp = subprocess.Popen('locate brew', shell=True, stdout=subprocess.PIPE)
+            # subprocess.Popen('kill -9' + str(i), shell=True, stdout=subprocess.PIPE)
+    else:
+        print("no sus task found")
+
+    # temp = subprocess.Popen('locate brew', shell=True, stdout=subprocess.PIPE)
     # get the output as a string
-    for i in temp.stdout:
-        outputlist.append(str(i.strip(), "utf-8"))
+    # for i in temp.stdout:
+    #    outputlist.append(str(i.strip(), "utf-8"))
     # store the output in the list
-
-    return outputlist
 
 
 def startNetworkTracking():
-    print("started network tracking")
     outputFileName = "nethogs.txt"
     process = subprocess.Popen("nethogs -t -v 2 > " + outputFileName, shell=True, stdin=subprocess.PIPE)
-    time.sleep(20)
-    process.kill()
-    print("--- finished network tracking ---")
+    time.sleep(60)
+    subprocess.Popen("pkill -f nethogs", shell=True, stdin=subprocess.PIPE)
     print("--- output saved in " + outputFileName)
 
 
 if __name__ == '__main__':
-    output = mitigateCryptojacker()
-
-    # Uncomment the following lines to print the output of successful servers
+    mitigateCryptojacker()
