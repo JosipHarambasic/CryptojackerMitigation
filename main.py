f6dadc0ac211eb864994aae4f315eb201671fe6a
Josip Harambasic <josip@Josips-MacBook-Pro.local>
2022-12-17 21:33:10 +0100

    update

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 import subprocess
 
-
 # sudo /usr/libexec/locate.updatedb
+import time
 
-def ping():
+from Parser import Parser
+
+
+def mitigateCryptojacker():
     outputlist = []
     # Iterate over all the servers in the list and ping each server
     sudo_password = '1230'
     command = 'updatedb'
     command = command.split()
 
-    #cmd1 = subprocess.Popen(['echo', sudo_password], stdout=subprocess.PIPE)
-    #subprocess.Popen(['sudo', '-S'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+    # Parse the output file that could have malicious programs
+
+    # We need to update the db to locate the actual target file
+    cmd1 = subprocess.Popen(['echo', sudo_password], stdout=subprocess.PIPE)
+    # subprocess.Popen(['sudo', '-S'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+
+    maliciousPrograms = Parser("whitelist.txt", "nethogs.txt").parse()
+    for i in maliciousPrograms:
+        # cpu limit 'cpulimit -p PID -l 10'
+        print(maliciousPrograms[i])
+        subprocess.Popen("cpulimit -p " + maliciousPrograms[i] + " -l 10")
+        # subprocess.Popen('kill -9' + str(i), shell=True, stdout=subprocess.PIPE)
+
     temp = subprocess.Popen('locate brew', shell=True, stdout=subprocess.PIPE)
     # get the output as a string
     for i in temp.stdout:
-        outputlist.append(i)
+        outputlist.append(str(i.strip(), "utf-8"))
     # store the output in the list
 
     return outputlist
 
 
+def startNetworkTracking():
+    print("started network tracking")
+    outputFileName = "nethogs.txt"
+    process = subprocess.Popen("nethogs -t -v 2 > " + outputFileName, shell=True, stdin=subprocess.PIPE)
+    time.sleep(20)
+    process.kill()
+    print("--- finished network tracking ---")
+    print("--- output saved in " + outputFileName)
+
+
 if __name__ == '__main__':
-
-    # Get the list of servers from the text file
-    servers = ["www.google.com", "www.facebook.com"]
-    # Iterate over all the servers that we read from the text file
-    # and remove all the extra lines. This is just a preprocessing step
-
-    outputlist = ping()
+    output = mitigateCryptojacker()
 
     # Uncomment the following lines to print the output of successful servers
-    print(outputlist)
-    for i in outputlist:
-        print(i)
